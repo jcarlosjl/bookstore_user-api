928fbbfec9844be8ca0368c0bbbdb56cab0034d0
Juan Carlos Juarez <juan.juarez@linio.com>
2020-02-04 09:06:18 -0600

    Create controller for users endpoint

---
+++ controllers/users/users_controller.go
@@ -0,0 +1,22 @@
+package users
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+//CreateUser method
+func CreateUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "Implement me!")
+}
+
+//GetUser method
+func GetUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "Implement me!")
+}
+
+//SearchUser method
+func SearchUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "Implement me!")
+}
